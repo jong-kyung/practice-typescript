c63b78416cb906c872627f06beaa4b8a046d0936
이종경 <gyoung3063413@naver.com>
2024-07-12 19:25:51 +0900

    add: null 단언

--- src/concept/25.NonNullable.ts
+++ src/concept/25.NonNullable.ts
@@ -3,3 +3,6 @@ type A = string | null | undefined | boolean | number;
 type B = NonNullable<A>;
 
 type N<T> = T extends null | undefined ? never : T;
+
+document.querySelector("input")!.id;
+// !를 사용하면 null이 아니라고 단언하는 것이다.
